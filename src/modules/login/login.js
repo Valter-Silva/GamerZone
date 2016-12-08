97935edcb6ddf12085c5dec1f5093029a1c23d68
DESKTOP-4M9THLT\Valter Silva <svalter.ribeiro@gmail.com>
2016-12-08 23:02:03 +0100

    resolved merge

--- src/modules/login/login.js
+++ src/modules/login/login.js
@@ -34,12 +34,10 @@ var login = {
 		var username = $$('#username').val();
 	    var password = $$('#password').val();
 	    // Handle username and password
-<<<<<<< HEAD
 	    //App.alert('Username: ' + username + ', Password: ' + password);
 	    if(appFunc.checkEquals(username, "S") && appFunc.checkEquals(password, "123")){
 	    	App.closeModal('#loginView');
 	    } 
-=======
 	    //hiApp.alert('Username: ' + username + ', Password: ' + password);
 
 	    var ok=false;
@@ -100,7 +98,6 @@ var login = {
 	    		hiApp.alert('There is an account related to that email!');
 	    	}
 	    });
->>>>>>> origin/Santi2
 	},
     bindEvents: function(){
         var bindings = [{
